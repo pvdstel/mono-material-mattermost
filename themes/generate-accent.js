3b8391981cc9d9b6a971ae04ba8a71a7e042b9be
Paul <pvdstel@users.noreply.github.com>
2018-04-03 00:19:01 +0200

    Add an accent generator script

---
+++ themes/generate-accent.js
@@ -0,0 +1,51 @@
+#!/usr/bin/env node
+
+
+// Parse arguments. Data arguments are non-option arguments
+const args = process.argv.slice(2);
+const dataArgs = [];
+for (let i = 0; i < args.length; ++i) {
+    const arg = args[i];
+    if (!arg.startsWith('-')) dataArgs.push(arg);
+}
+
+if (dataArgs.length < 2) {
+    console.log("At least two data arguments are required: a theme and the accent color.");
+    process.exit(1);
+}
+
+
+// Define helper functions
+const makeColorValue = (color) => color.startsWith('#') ? color : `#${color}`;
+
+
+// Define what should be accented
+const accented = ['sidebarTextActiveBorder', 'linkColor', 'buttonBg'];
+
+
+// Load the theme
+const theme = require(`./${args[0]}`);
+
+
+// Replace accented theme parts
+const replaceAll = args.some(a => a === '-a' || a === '--all');
+if (replaceAll) {
+    for (let accent of accented) {
+        theme[accent] = makeColorValue(dataArgs[1]);
+    }
+} else {
+    const maxReplace = Math.min(accented.length, dataArgs.length - 1);
+    for (let i = 0; i < maxReplace; ++i) {
+        const accent = accented[i];
+        theme[accent] = makeColorValue(dataArgs[1 + i]);
+    }
+}
+
+
+// Prettify output if requested
+const isPretty = args.some(a => a === '-p' || a === '--pretty');
+if (isPretty) {
+    console.log(JSON.stringify(theme, null, 4));
+} else {
+    console.log(JSON.stringify(theme));
+}
